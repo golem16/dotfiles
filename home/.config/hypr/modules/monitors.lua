14b4df51e48c4449ce1672b642803edf91d44847
golem <259251440+golem16@users.noreply.github.com>
2026-08-22 16:15:15 -0400

    Hypr: Improved monitor setup + monitor for desktop

--- home/.config/hypr/modules/monitors.lua
+++ home/.config/hypr/modules/monitors.lua
@@ -3,7 +3,17 @@ hl.monitor({
   mode = "2560x1600@165",
   cm = "dcip3",
   vrr = 2,
-  scale = 1.0
+  scale = 1.0,
+  icc = "/usr/share/color/icc/colord/BOE_CQ_______NE160QDM_NZ6.icm"
 })
 
--- hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor({
+  output = "DP-1",
+  mode = "2560x1440@240",
+  cm = "hdr",
+  vrr = 2,
+  scale = 1.0,
+  bitdepth = 10,
+  sdrbrightness = 2.0,
+  sdrsaturation = 1.25
+})
